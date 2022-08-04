9fcd3be1a4c6811de2e828cf2b315cdd1708c9da
jumashaggy <juancliment@gmail.com>
2022-08-04 16:21:36 -0300

    sea agrego los link de los html y se crearon los css de cada pagina

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+app.use(express.static('public'));
 
 //---------------Recursos estáticos----
 app.use(express.static("public"));
 
 //---------------Servidor local-----
 
-app.listen(3050, ()=>console.log ("Server running on port 3050"));
+app.listen(3000, ()=>console.log ("Server running on port 3000"));
 
 //---------------Ruteo Home-----
 
 app.get("/", function (req, res){
 
     res.sendFile(path.join(__dirname, "views/home_weiss.html"))
+
+});
+app.get("/carrito_compras", function (req, res){
+
+    res.sendFile(path.join(__dirname, "views/carrito_compras.html"))
+
+});
+app.get("/productos", function (req, res){
+
+    res.sendFile(path.join(__dirname, "views/productos.html"))
+
+});
+app.get("/login", function (req, res){
+
+    res.sendFile(path.join(__dirname, "views/login.html"))
+
+});
+app.get("/registro", function (req, res){
+
+    res.sendFile(path.join(__dirname, "views/registro.html"))
 
 });
