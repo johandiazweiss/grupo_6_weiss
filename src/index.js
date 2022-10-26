471d73135aec3c5b6dfd9ccb42e644f062fb766e
Johan Diaz <johandiaz@MacBook-Pro-de-Johan.local>
2022-10-26 20:20:08 -0300

    heroku

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(methodOverride ("_method"));
 
 //---------------Servidor local-----
 
-app.listen(3000, ()=>console.log ("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, ()=>console.log ("Server running on port 3000"));
 
 //---------------Routing-----
 
